68f3cb168695ad3bb4a787b1a9f4c8add21b8ca7
SHAKEEB KHAN <shakeebsun@gmail.com>
2023-02-07 17:31:34 +0500

    Initial commit

---
+++ api/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+# Create your models here.
+# class User(models.Model):
+#     id = models.CharField()
+#     username = models.CharField()
+#     email = models.EmailField()
